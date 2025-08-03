6a925e136b5bdc4a0d669466b5f9fc96a574e6f8
LeeKeonSoo <ksl1165@naver.com>
2025-08-03 16:07:08 +0900

    동일 commit.

--- main_experiment.py
+++ main_experiment.py
@@ -113,6 +113,7 @@ def create_optimizers_config(base_lr: float, weight_decay: float) -> dict:
         'AdamABS': {
             'optimizer_class': CustomAdamABS,
             'params': {
+                'lr': base_lr,
                 'betas': (0.9, 0.999),
                 'eps': 1e-8,
                 'weight_decay': weight_decay
